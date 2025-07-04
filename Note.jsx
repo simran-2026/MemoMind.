393f08908a7a0e82cd9d23ee2b5b9fc8d68da2df
simran verma <vermasimran327@gmail.com>
2025-07-04 17:47:08 +0530

    Create Note.jsx

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <note>
+      <div className="note">
+        <h1>This is our Memomind</h1>
+        <p>
+          {" "}
+          MemoMind – Where your thoughts dance and memories find their cozy
+          home! A little nest for your ideas, dreams, and soulful scribbles
+        </p>
+      </div>
+    </note>
+  );
+}
+
+export default Note;
